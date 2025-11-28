12edbf5b2beda68e2027885dd8327079c5fba001
olhkyle <kylekwon.dev@gmail.com>
2025-11-28 16:15:06 +0900

    refactor : refactor mongoose.ts

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,42 +1,18 @@
 import mongoose from 'mongoose';
 
-// sustain mongodb pool connection on Vercel / Serverless
-type MongooseCache = {
-	conn: typeof mongoose | null;
-	promise: Promise<typeof mongoose> | null;
-};
-
-const globalCache = globalThis as typeof globalThis & { mongoose?: MongooseCache };
-if (!globalCache.mongoose) {
-	globalCache.mongoose = { conn: null, promise: null };
-}
-
-const cached = globalCache.mongoose;
-
 export const connectDB = async () => {
-	if (cached.conn) {
-		if (globalCache.mongoose?.conn?.connection.readyState) {
+	try {
+		if (mongoose.connection.readyState !== 0) {
 			console.log('Already Connected to DB');
-			return cached.conn;
+			return;
 		}
+
+		await mongoose.connect(process.env.MONGODB_URI || '', {
+			dbName: process.env.MONGODB_DB_NAME!,
+		});
+		console.log('Connected to DB');
+	} catch (error) {
+		console.log('Error happened to connect DB', error);
+		throw new Error(String(error));
 	}
-
-	if (!cached.promise) {
-		cached.promise = mongoose
-			.connect(process.env.MONGODB_URI!, {
-				dbName: process.env.MONGODB_DB_NAME!,
-				maxPoolSize: 20,
-				minPoolSize: 5, // 중요
-			})
-			.then(m => m)
-			.catch(error => {
-				console.log('Error happened to connect DB', error);
-				throw new Error(String(error));
-			});
-
-		console.log('Connected To DB');
-	}
-
-	cached.conn = await cached.promise;
-	return cached.conn;
 };
