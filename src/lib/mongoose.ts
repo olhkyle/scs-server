858edc1582c3f0bd3adbc735b2a02c44944523bd
olhkyle <itskkyle@gmail.com>
2025-12-10 13:33:05 +0900

    feat : update mongoose.connection.readyState

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
 	try {
-		if (mongoose.connection.readyState !== 0) {
+		if (mongoose.connection.readyState === 1) {
 			console.log('Already Connected to DB');
 			return;
 		}
@@ -10,6 +10,7 @@ export const connectDB = async () => {
 		await mongoose.connect(process.env.MONGODB_URI || '', {
 			dbName: process.env.MONGODB_DB_NAME!,
 		});
+
 		console.log('Connected to DB');
 	} catch (error) {
 		console.log('Error happened to connect DB', error);
